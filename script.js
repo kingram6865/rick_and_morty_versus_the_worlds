716a9e5167f0bf13d9bf44881c4595c078b3b718
Ken Ingram <ken.ingram@gmail.com>
2020-08-17 14:51:21 -0700

    Using a recursion pattern to retrieve additional data.

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
-const API='https://rickandmortyapi.com/api/character/2'
+const API='https://rickandmortyapi.com/api/'
+const allCharacters = "https://rickandmortyapi.com/api/character/"
 const universePeople = [] // All the available characters in the Rick and Morty universe
 const universeWorlds = []
 
 
-async function populatePeople(){ // Retrieve the data from the API
-	const allCharacters = "https://rickandmortyapi.com/api/character/"
+async function populatePeople(info){ // Retrieve the data from the API
 	try {
-		let characters = await axios.get(allCharacters)
+		let characters = await axios.get(info)
 			.then((res)=>{
-				console.log(res.data)
-				// console.log(res.data.Search)
+				console.log(res)
+				//res.data.next
+				console.log(res.data.Search)
+				if (res.data.info.next){
+					populatePeople(res.data.info.next)
+				}
+				
+				for (let i=0; i < res.data.results.length; i++){
+					universePeople.push(res.data.results[i])
+					//console.log(res.data.results[i])
+				}
+
+				console.log(`${universePeople.length} charcters retrieved...`)
 			})		
 	} catch (error) {
-		console.log(`Error: ${error`)
+		console.log(`Error: ${error}`)
 	}
 
-
-
-
 }
+
+populatePeople(allCharacters)
+
+async function apiCall(url, options){}

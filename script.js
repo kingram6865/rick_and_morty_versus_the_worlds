6524b8ccc36cc79034479008cdc9f7e76580383d
Ken Ingram <ken.ingram@gmail.com>
2020-08-17 20:51:42 -0700

    Trying to figure out this API's returns

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const API='https://rickandmortyapi.com/api/'
 const allCharacters = "https://rickandmortyapi.com/api/character/"
 const universePeople = [] // All the available characters in the Rick and Morty universe
 const universeWorlds = []
-
+const chooseRickMorty = document.createElement('select')
 
 async function populatePeople(info){ // Retrieve the data from the API
 	let allPeople = []
@@ -15,6 +15,7 @@ async function populatePeople(info){ // Retrieve the data from the API
 				// console.log(res.data.Search)
 				for (let i=0; i < res.data.results.length; i++){
 					allPeople.push(res.data.results[i])
+					console.log(allPeople.length)
 					//console.log(res.data.results[i])
 				}
 
@@ -22,17 +23,27 @@ async function populatePeople(info){ // Retrieve the data from the API
 					allPeople.push(populatePeople(res.data.info.next))
 				}
 
-				return allPeople
-			})		
+			})
+
+			return characters		
 	} catch (error) {
 		console.log(`Error: ${error}`)
-	} finally {
-		console.log(`Finally: ${allPeople.length} charcters retrieved...`)
 	}
+	// } finally {
+	// 	// console.log(`Finally: ${allPeople.length} charcters retrieved...`)
+	// }
 
-	console.log(`Function End: ${allPeople.length} charcters retrieved...`)
+	// console.log(`Function End: ${allPeople.length} charcters retrieved...`)
+	console.log(characters)
 }
 
 populatePeople(allCharacters)
+
+
+function updateUniverseList(data){
+	universePeople.push(data)
+	console.log(universePeople.length)
+}
+
 
 async function apiCall(url, options){}
